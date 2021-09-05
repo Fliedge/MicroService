71c0017ffd20bd633bdf4b6d922cdb9624e00e05
Filip Aldenhov <74648607+Fliedge@users.noreply.github.com>
2021-09-05 18:05:07 +0200

    Add files via upload

---
+++ user-service/src/main/java/com/fwellep/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.fwellep.user.repository;
+
+import com.fwellep.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserId(Long userId);
+}
